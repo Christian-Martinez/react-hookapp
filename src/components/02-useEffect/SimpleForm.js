9cb56bb7d9da2f22dc607e6e1242bd5aa9ce9eba
Christian-Martinez <christ96mart@gmail.com>
2021-03-04 22:48:23 -0400

    simple form con useEffect

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+//import { Message } from './Message';
+
+import './effect.css';
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+  });
+
+  const { name, email } = formState;
+
+  //cargara solo una vez
+  useEffect(() => {
+    console.log('Cargó por primera vez');
+  }, []);
+
+  useEffect(() => {
+    console.log('formState cambió');
+  }, [formState]);
+
+  //util para un selector, combobox
+  useEffect(() => {
+    console.log('email cambió');
+  }, [email]);
+
+  const handleInputChange = ({ target }) => {
+    //console.log(target);
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+      <div className='form-group'>
+        <input
+          type='text'
+          name='name'
+          className='form-control'
+          placeholder='Tu nombre'
+          autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className='form-group'>
+        <input
+          type='text'
+          name='email'
+          className='form-control'
+          placeholder='email@gmail.com'
+          autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+};
